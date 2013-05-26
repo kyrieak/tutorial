e929a72307e844a7d075b97748c759356758179e
Kyrie <kyrie.ak@gmail.com>
2013-05-25 17:10:53 -0700

    added bunny route test

--- test/BunnyTest.java
+++ test/BunnyTest.java
@@ -1,15 +1,21 @@
 import org.junit.Test;
+
+import play.libs.F.Callback;
 import play.mvc.Result;
+import play.test.TestBrowser;
 
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 
 public class BunnyTest {
 
-    @Test
-    public void testFooRoute() {
-        Result result = route(fakeRequest(GET, "/bunny"));
-        assertThat(result).isNotNull();
-    }
-
+	@Test
+	public void testBunnyRoute() {
+		running(fakeApplication(), new Runnable() {
+			public void run() {
+				Result result = route(fakeRequest(GET, "/bunny"));
+				assertThat(result).isNotNull();;
+			}
+		});
+	}
 }
