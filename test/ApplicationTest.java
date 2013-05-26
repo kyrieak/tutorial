ea8e06fe880e55c89a5e05c4b226e68539985f30
Kyrie <kyrie.ak@gmail.com>
2013-05-25 17:27:22 -0700

    added controller test for index

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,3 +1,6 @@
+import org.junit.Test;
+import play.mvc.Result;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,12 +22,10 @@ import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 
 
-/**
-*
-* Simple (JUnit) tests that can call all parts of a play app.
-* If you are interested in mocking a whole application, see the wiki for more details.
-*
-*/
+import static org.fest.assertions.Assertions.assertThat;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.*;
+
 public class ApplicationTest {
 
     @Test 
@@ -35,10 +36,22 @@ public class ApplicationTest {
     
     @Test
     public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
+        Content html = views.html.index.render("Bunny says hi.");
         assertThat(contentType(html)).isEqualTo("text/html");
-        assertThat(contentAsString(html)).contains("Your new application is ready.");
+        assertThat(contentAsString(html)).contains("Bunny says hi.");
     }
   
-   
+    @Test
+    
+    public void callIndex() {
+    	running(fakeApplication(), new Runnable() {
+    		public void run() {
+    			Result result = callAction(controllers.routes.ref.Application.index());
+    			assertThat(status(result)).isEqualTo(OK);
+    			assertThat(contentType(result)).isEqualTo("text/html");
+    			assertThat(charset(result)).isEqualTo("utf-8");
+    			assertThat(contentAsString(result)).contains("Bunny says hi.");
+    		}
+    	});
+    }
 }
